425564419ae4b6740ba863904d371ccf38b1a687
Om Sinkar <omsinkar03bit@gmail.com>
2023-05-08 22:44:47 +0530

    v0.0.1: Multiplayer Tic Tac Toe

---
+++ assets/js/app.js
@@ -0,0 +1,63 @@
+let count = 0;
+var box = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+function boxclick(boxid){
+    let index = boxid.id;
+    if(box[index] === 0){
+        if(count%2 === 0){   
+            var img = document.createElement("img");
+            img.src = "../assets/Images/o.png";
+            img.style.width = "35px";
+            img.style.height = "35px";
+            img.style.margin = "4vh auto";
+            boxid.appendChild(img); 
+            box[index] = 1;
+        }
+        else{
+            var img = document.createElement("img");
+            img.src = "../assets/Images/x.png";
+            img.style.width = "35px";
+            img.style.height = "35px";
+            img.style.margin = "4vh auto";
+            boxid.appendChild(img); 
+            box[index] = 2;
+        }
+        check();
+        count++;
+    }
+}
+const compareArrays = (a, b) => {
+    return JSON.stringify(a) == JSON.stringify(b);
+};
+function check(){
+    let x = 1;
+    let flag = 0;
+    if((box[0]===box[1] && box[1] === box[2] && box[2] == x) || (box[3]===box[4] && box[4] === box[5] && box[5] == x) || (box[6]===box[7] && box[7] === box[8] && box[8] == x) || (box[0]===box[3] && box[3] === box[6] && box[6] == x) || (box[1]===box[4] && box[4] === box[7] && box[7] == x) || (box[2]===box[5] && box[5] === box[8] && box[8] == x) || (box[0]===box[4] && box[4] === box[8] && box[8] == x) || (box[2]===box[4] && box[4] === box[6] && box[6] == x)){
+        // document.getElementById("page-heading").style.visibility = "hidden";
+        // document.getElementById("game-set").style.visibility = "hidden";
+        document.getElementById("win-page").style.visibility = "visible";
+        document.getElementById('win-page').style.display = "content";
+        document.getElementById('win-text').innerHTML = "O Wins";
+        console.log("O Win");
+        flag = 1
+    }
+    x = 2;
+    if((box[0]===box[1] && box[1] === box[2] && box[2] == x) || (box[3]===box[4] && box[4] === box[5] && box[5] == x) || (box[6]===box[7] && box[7] === box[8] && box[8] == x) || (box[0]===box[3] && box[3] === box[6] && box[6] == x) || (box[1]===box[4] && box[4] === box[7] && box[7] == x) || (box[2]===box[5] && box[5] === box[8] && box[8] == x) || (box[0]===box[4] && box[4] === box[8] && box[8] == x) || (box[2]===box[4] && box[4] === box[6] && box[6] == x)){
+        // document.getElementById("page-heading").style.visibility = "hidden";
+        // document.getElementById("game-set").style.visibility = "hidden";
+        document.getElementById("win-page").style.visibility = "visible";
+        document.getElementById('win-page').style.display = "content";
+        document.getElementById('win-text').innerHTML = "X Wins";
+        flag = 2;
+    }
+    
+    for(var i = 0; i<9; i++){
+        if(box[i] == 0){
+            flag = -1;
+        }
+    }
+    if(flag==0){
+        document.getElementById("win-page").style.visibility = "visible";
+        document.getElementById('win-page').style.display = "content";
+        document.getElementById('win-text').innerHTML = "Tie";
+   }
+}
---
+++ assets/js/loading.js
@@ -0,0 +1,50 @@
+let audio = document.getElementById('loadaudio');
+console.log(audio);
+function loadpage(){
+    document.getElementById('btn-play').style.display = "none";
+    document.getElementById('btn-play').style.visibility = "hidden";
+    // document.getElementById('maindiv').style.display = "content";
+    // document.getElementById('maindiv').style.visibility = "visible";
+    showdiv1();
+}
+function showdiv1() {
+    setTimeout(function () {
+        document.getElementById("Dline1").style.visibility = "visible";
+        // document.getElementById('Dline2').style.display = "content";
+        audio.play(); 
+        // console.log("1");
+    }, 1000);
+    setTimeout(function () {
+        document.getElementById("Dline2").style.visibility = "visible";
+        // document.getElementById('Dline2').style.display = "content";
+        audio.play(); 
+        // console.log("1");
+    }, 2000);
+    setTimeout(function () {
+        document.getElementById("x1").style.visibility = "visible";
+        document.getElementById("x2").style.visibility = "visible";
+        document.getElementById("o1").style.visibility = "visible";
+        document.getElementById("o2").style.visibility = "visible";
+
+        
+        // document.getElementById('Dline2').style.display = "content";
+        audio.play(); 
+        // console.log("1");
+    }, 3000);
+    setTimeout(function () {
+        document.getElementById("Dline1").style.visibility = "hidden";
+        document.getElementById("Dline2").style.visibility = "hidden";
+        document.getElementById("x1").style.visibility = "hidden";
+        document.getElementById("x2").style.visibility = "hidden";
+        document.getElementById("o1").style.visibility = "hidden";
+        document.getElementById("o2").style.visibility = "hidden";
+        // document.getElementById("page-heading").style.visibility = "visible";
+        // document.getElementById("game-set").style.visibility = "visible";
+        window.location.href = "index.html";
+    }, 4000);
+    
+
+}
+function refresh(){
+    location.reload();
+};
